ef052e67af5935291709e3caed92d3617477c024
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-11-15 12:17:12 -0800

    simple_shell

--- errors1.c
+++ errors1.c
@@ -120,7 +120,7 @@ char *convert_number(long int num, int base, int flags)
 }
 
 /**
- * remove_comments - function replaces first instance of '#' with '\0'
+ * remove_comment - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
  *
  * Return: Always 0;
