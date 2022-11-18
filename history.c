ebe3a829c66cf55a59c81f35031b9eec079fe741
CynthiaAdesuwa <cynthiaadesuwaosagie@gmail.com>
2022-11-18 15:09:41 -0800

    simple_shell

---
+++ history.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ * get_history_file - gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
+char *get_history_file(info_t *info)
