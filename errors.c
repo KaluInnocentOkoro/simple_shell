8bfb715cb2721ad3de1c0bec382e08468cb7b3fa
Cynthia Osagie <110992792+CynthiaAdesuwa@users.noreply.github.com>
2022-11-18 23:54:27 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,8 +1,9 @@
 #include "shell.h"
 
 /**
- * _eputs - prints an input string
+ *_eputs - prints an input string
  * @str: the string to be printed
+ *
  * Return: Nothing
  */
 void _eputs(char *str)
@@ -21,6 +22,7 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -43,6 +45,7 @@ int _eputchar(char c)
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -62,7 +65,7 @@ int _putfd(char c, int fd)
 }
 
 /**
- * _putsfd - prints an input string
+ *_putsfd - prints an input string
  * @str: the string to be printed
  * @fd: the filedescriptor to write to
  *
