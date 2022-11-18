99d4d347b832b59a91c4620ff9987cee9e61bbf8
Cynthia Osagie <110992792+CynthiaAdesuwa@users.noreply.github.com>
2022-11-19 00:22:49 +0100

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -54,9 +54,11 @@ char **list_to_strings(list_t *head)
 	return (strs);
 }
 
+
 /**
  * print_list - prints all elements of a list_t linked list
  * @h: pointer to first node
+ *
  * Return: size of list
  */
 size_t print_list(const list_t *h)
@@ -77,11 +79,12 @@ size_t print_list(const list_t *h)
 }
 
 /**
- *  node_starts_with - returns node whose string starts with prefix
- *  @node: pointer to list head
- *  @prefix: string to match
- *  @c: the next character after prefix to match
- *  Return: match node or null
+ * node_starts_with - returns node whose string starts with prefix
+ * @node: pointer to list head
+ * @prefix: string to match
+ * @c: the next character after prefix to match
+ *
+ * Return: match node or null
  */
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
@@ -100,8 +103,9 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 /**
  * get_node_index - gets the index of a node
  * @head: pointer to list head
- *  @node: pointer to the node
- *  Return: index of node or -1
+ * @node: pointer to the node
+ *
+ * Return: index of node or -1
  */
 ssize_t get_node_index(list_t *head, list_t *node)
 {
